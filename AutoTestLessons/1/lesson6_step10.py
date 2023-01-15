5a8fde66988ea4e05ab36fb9ff3e890d424fc641
Aleksylt <90978623+Aleksylt@users.noreply.github.com>
2023-01-15 21:58:51 +0300

    2 день

---
+++ AutoTestLessons/1/lesson6_step10.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+
+
+
+
+#############проверка обязательных полей
+    browser.get(link)
+    first_name = browser.find_element(By.CSS_SELECTOR, "input[required].first")
+    first_name.send_keys("first_name")
+    last_name = browser.find_element(By.CSS_SELECTOR, "input[required].second")
+    last_name.send_keys("last_name")
+    email = browser.find_element(By.CSS_SELECTOR, "input[required].third")
+    email.send_keys("a@b.c")
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
