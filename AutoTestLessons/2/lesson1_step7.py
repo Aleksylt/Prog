dc443fb82660ef5d32a4374661a2b2b38bd605ed
Aleksylt <90978623+Aleksylt@users.noreply.github.com>
2023-01-27 20:39:56 +0300

    next lessons

---
+++ AutoTestLessons/2/lesson1_step7.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time, math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+
+    browser.get(link)
+    x_element = browser.find_element(By.CSS_SELECTOR, "#treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    answer = browser.find_element(By.CSS_SELECTOR, "#answer")
+    answer.send_keys(y)
+    c_box = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    c_box.click()
+
+    r_but = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    r_but.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-default")
+    button.click()
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
